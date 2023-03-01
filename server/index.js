db810bd3537389ee8fad2c617a9c200ae9f47a02
Jesse Dagenais <jessedagenais91@gmail.com>
2023-03-01 18:57:12 -0500

    Starter

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port = 8000;
+
+express()
+  .use(express.json())
+  .get('/hello', (req, res) => {
+    res.status(200).json({ status: 200, message: 'WhATUPDAwg' });
+  })
+
+  .listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
